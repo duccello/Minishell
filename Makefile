54440738212b5df1041b41d854bb748271f8e12b
Simone Gasparini <sgaspari@c3a1c4.42berlin.de>
2025-08-21 15:17:07 +0200

    Update main branch

--- Makefile
+++ Makefile
@@ -1,19 +1,21 @@
 CC 			= 	cc
-FLAGS		=	-g -Wall -Wextra -Werror -lreadline
+FLAGS		=	-g -Wall -Wextra -Werror 
 INCS		=	-Iinc											\
 				-Ilibft/inc
 SRC_DIR		=	src
 OBJ_DIR		=	build
-SRCS		=	main.c
+SRCS		=	main.c											\
+				cmd.c
 SRC_FILES	=	$(addprefix $(SRC_DIR)/, $(SRCS))
 OBJS		=	$(patsubst $(SRC_DIR)/%.c, $(OBJ_DIR)/%.o, $(SRC_FILES))
 LIBFT		=	-Llibft -lft
+READLINE	=	-lreadline
 NAME		=	minishell
 
 all:		$(NAME)
 
 $(NAME):	$(OBJS)
-	@$(CC) $(OBJS) -o $(NAME)
+	@$(CC) $(OBJS) $(LIBFT) $(READLINE) -o $(NAME)
 
 $(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
 	@mkdir -p $(OBJ_DIR)
--- inc/cmd.h
+++ inc/cmd.h
@@ -1,22 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   structs.h                                          :+:      :+:    :+:   */
+/*   cmd.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sgaspari <sgaspari@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/21 14:28:37 by sgaspari          #+#    #+#             */
-/*   Updated: 2025/08/21 15:01:30 by sgaspari         ###   ########.fr       */
+/*   Created: 2025/08/21 15:01:55 by sgaspari          #+#    #+#             */
+/*   Updated: 2025/08/21 15:02:30 by sgaspari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef STRUCTS_H
-# define STRUCTS_H
+#ifndef CMD_H
+# define CMD_H
 
-typedef struct s_cmd
-{
-	char	*path;
-	char	**argv;
-} t_cmd;
+t_cmd	*create_cmd(char *s);
 
 #endif
