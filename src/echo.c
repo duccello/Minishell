87fad200efd7dab72e1fbc4b4e0d981044f8d13c
Simone Gasparini <sgaspari@c3a2c7.42berlin.de>
2025-08-25 12:28:43 +0200

    djsdkjas

--- src/echo.c
+++ src/echo.c
@@ -6,7 +6,7 @@
 /*   By: sgaspari <sgaspari@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 12:24:36 by sgaspari          #+#    #+#             */
-/*   Updated: 2025/08/25 12:23:39 by sgaspari         ###   ########.fr       */
+/*   Updated: 2025/08/25 12:25:38 by sgaspari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@
 #include "macros.h"
 #include "pipex.h"
 
-void	echo(t_pipe *data)
+void	echo(t_pipe *p)
 {
-	if (ft_strncmp(data->cmd->argv[1], "-n",
-			ft_strlen(data->cmd->argv[1]) + 1) == 0)
-		printf("%s", data->cmd->argv[2]);
+	if (ft_strncmp(p->cmd[1], "-n",
+			ft_strlen(p->cmd[1]) + 1) == 0)
+		printf("%s", p->cmd[1]);
 	else
-		printf("%s\n", data->cmd->argv[1]);
+		printf("%s\n", p->cmd[1]);
 }
--- src/export.c
+++ src/export.c
@@ -6,17 +6,17 @@
 /*   By: sgaspari <sgaspari@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 12:48:38 by sgaspari          #+#    #+#             */
-/*   Updated: 2025/08/23 12:52:58 by sgaspari         ###   ########.fr       */
+/*   Updated: 2025/08/25 12:27:51 by sgaspari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "list.h"
 #include "structs.h"
 
-void	export(t_data *data)
+void	export(t_data *data, t_pipe *p)
 {
 	t_node	*node;
 
-	node = create_node(data->cmd->argv[1]);
+	node = create_node(p->[1]);
 	append_node(&data->envp, node);
 }
