2630f2f0635fbbc6acc13acddc2583c14a28ab74
Simone Gasparini <sgaspari@c3a2c7.42berlin.de>
2025-09-04 15:44:18 +0200

    Signals status are correct

--- src/signals.c
+++ src/signals.c
@@ -6,7 +6,7 @@
 /*   By: sgaspari <sgaspari@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/25 15:12:53 by sgaspari          #+#    #+#             */
-/*   Updated: 2025/09/04 13:11:57 by sgaspari         ###   ########.fr       */
+/*   Updated: 2025/09/04 15:37:17 by sgaspari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,11 @@ void	handle_signals(void)
 
 	sa_sigint.sa_handler = handle_sigint;
 	sigemptyset(&sa_sigint.sa_mask);
+	sa_sigint.sa_flags = SA_RESTART;
 	sigaction(SIGINT, &sa_sigint, NULL);
 	sa_sigquit.sa_handler = SIG_IGN;
 	sigemptyset(&sa_sigquit.sa_mask);
+	sa_sigquit.sa_flags = SA_RESTART;
 	sigaction(SIGQUIT, &sa_sigquit, NULL);
 }
 
